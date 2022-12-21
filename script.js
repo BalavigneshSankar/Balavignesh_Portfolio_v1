ce48f09360cb40255bd7b0e46648120e298f8316
BalavigneshSankar <balavignesh.shankar@gmail.com>
2022-12-21 19:39:17 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+"use strict";
+
+// Smooth scroll to respective section on clicking navigation link.
+document.querySelector(".main-nav").addEventListener("click", (e) => {
+  e.preventDefault();
+
+  if (e.target.classList.contains("main-nav-link")) {
+    const id = e.target.getAttribute("href");
+    document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+  }
+});
+
+// Smooth scroll to contact section on clicking "Connect to me" button.
+document.querySelector(".link-contact").addEventListener("click", function (e) {
+  e.preventDefault();
+
+  const id = this.getAttribute("href");
+  document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+});

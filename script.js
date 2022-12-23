8b6594ac18cf308eadbd8cb4c4caac454777f7b9
BalavigneshSankar <balavignesh.shankar@gmail.com>
2022-12-23 11:33:10 +0530

    Implemented sticky navigation bar by IntersectionObserver API

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 "use strict";
-
 // Smooth scroll to respective section on clicking navigation link.
 document.querySelector(".main-nav").addEventListener("click", (e) => {
   e.preventDefault();
@@ -17,3 +16,26 @@ document.querySelector(".link-contact").addEventListener("click", function (e) {
   const id = this.getAttribute("href");
   document.querySelector(id).scrollIntoView({ behavior: "smooth" });
 });
+
+// Sticky navigation bar using IntersectionObserver API
+const header = document.querySelector(".header");
+const headerHeight = header.getBoundingClientRect().height;
+
+const obsCallback = (entries, observer) => {
+  const [entry] = entries;
+  if (!entry.isIntersecting) {
+    header.classList.add("sticky");
+  }
+  if (entry.isIntersecting) {
+    header.classList.remove("sticky");
+  }
+};
+
+const obsOptions = {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${headerHeight}px`,
+};
+
+const observer = new IntersectionObserver(obsCallback, obsOptions);
+observer.observe(document.querySelector(".section-hero"));
